fd5c21d243471cac4b086c921a33448bda15636f
marianodato <marianodato@gmail.com>
2017-09-08 23:17:21 -0300

    Add https force redirect

--- grails-app/conf/webserver/SecurityFilters.groovy
+++ grails-app/conf/webserver/SecurityFilters.groovy
@@ -5,10 +5,18 @@ import grails.util.Environment
 class SecurityFilters {
 
     def filters = {
-        switchToHttps(uri: "/**") {
+        switchToHttps(controller:'*', action:'*') {
             before = {
+
+                log.info("isSecure: " + request.isSecure())
+                log.info("isDevelopmentMode : " + Environment.isDevelopmentMode())
+                log.info("serverName: " +  request.serverName)
+                log.info("forwardURI: " +  request.forwardURI)
+                log.info("queryString: " +  request.queryString)
+
                 if (!request.isSecure() && !Environment.isDevelopmentMode()) {
                     def url = "https://" + request.serverName + request.forwardURI + (request.queryString ? "?" + request.queryString : "")
+                    log.info("Redirect url: " +  url)
                     redirect(url: url, permanent: true)
                     return false
                 }
